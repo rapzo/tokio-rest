33b73da66231a70929f10e31258227ed4d19f268
Jorge Silva <jorge.silva@blip.pt>
2016-11-27 17:09:13 +0000

    Add more comments to server
    Rearranged some variables and even deleted one not reference elsewhere.
    Added comments to aid those who read the code.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 'use strict';
 
 const koa = require('koa');
-const Promise = require('bluebird');
 const uuid = require('uuid').v4;
+const Promise = require('bluebird');
 const toobusy = require('toobusy-js');
-const pathMatch = require('koa-path-match')
+const match = require('koa-path-match')();
+const bodyParser = require('koa-bodyparser');
 
 module.exports = function serve(container, opts) {
 
     return new Promise((resolve, reject) => {
         const app = koa();
-        const match = pathMatch();
-        const bodyParser = require('koa-bodyparser');
-
+        
         // Error handler
+        // TODO: improve
         app.on('error', function(err) {
             if (process.env.NODE_ENV != 'production') {
-                console.error(err);
+                container.logger(err);
             }
         });
 
@@ -42,7 +42,7 @@ module.exports = function serve(container, opts) {
             }));
         }
 
-        // logger
+        // TODO: Request logger
         /*app.use(function *(next){
             var start = new Date;
             yield next;
@@ -50,21 +50,31 @@ module.exports = function serve(container, opts) {
             console.log('%s %s - %s', this.method, this.url, ms);
         });*/
 
-        // response
+        // API middleware
         app.use(match(opts.route, function *() {
             const ctx = {
                 $cid: uuid(),
+                // Not suitable for JSON.stringify
                 $requestContext: {
+                    // {Object} - Request headers
                     headers: this.request.header,
+                    // {see https://www.npmjs.com/package/cookies} - Lazy loaded cookies
+                    cookies: this.cookies,
+                    // {Object} - URL params
                     params: this.params,
+                    // {Object} - Query parameters
                     query: this.query,
+                    // {Object} - Parsed body content
                     body: this.request.body
                 },
+                // Koa request wrapper
                 $req: this.request,
+                // Koa response wrapper
                 $res: this.response
-            };            
+            };
 
             try {
+                // Fulfill request
                 let result = yield container.execute(ctx);
 
                 // Result may have been set inside the program by using $res.body
